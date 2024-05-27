d3aa356d2feb34645a6e68bc9a726357cbe96081
Hiroznkkj <97319174+Hiroznkkj@users.noreply.github.com>
2024-05-27 14:57:23 -0300

    Remove the canvas api

--- my-api/index.js
+++ my-api/index.js
@@ -5,12 +5,11 @@ const path = require('path');
 // Importar rotas
 const indexRouter = require('./routes/index');
 const apiRouter = require('./routes/api');
-const canvasRouter = require('./routes/canvas');
+
 
 // Usar as rotas importadas
 app.use('/', indexRouter);
 app.use('/api', apiRouter);
-app.use('/api/canvas', canvasRouter);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, '0.0.0.0', () => {
--- my-api/routes/api.js
+++ my-api/routes/api.js
@@ -1,17 +1,54 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.send('hello world');
-});
 
 // Adicionando mais rotas na API
 router.get('/users', (req, res) => {
   res.send('List of users');
 });
 
-router.get('/products', (req, res) => {
-  res.send('List of products');
+router.get('/', (req, res) => {
+  const text = req.query.text || 'Empty querry';
+  res.send(text);
 });
+
+router.get('/teste', (req, res) => {
+  const width = 800;
+  const height = 600;
+  const canvas = createCanvas(width, height);
+  const ctx = canvas.getContext('2d');
+
+  // Exemplo de desenho no canvas
+  ctx.fillStyle = 'skyblue';
+  ctx.fillRect(0, 0, width, height);
+
+  ctx.fillStyle = 'black';
+  ctx.font = '40px Arial';
+  ctx.fillText('Hello, Canvas!', 200, 300);
+
+  // Enviar a imagem como resposta
+  res.setHeader('Content-Type', 'image/png');
+  canvas.pngStream().pipe(res);
+});
+
+router.get('/querry', (req, res) => {
+    const width = parseInt(req.query.width) || 800;
+    const height = parseInt(req.query.height) || 600;
+    const text = req.query.text || 'Hello, Canvas!';
+    const canvas = createCanvas(width, height);
+    const ctx = canvas.getContext('2d');
+  
+    // Exemplo de desenho no canvas
+    ctx.fillStyle = 'skyblue';
+    ctx.fillRect(0, 0, width, height);
+  
+    ctx.fillStyle = 'black';
+    ctx.font = '40px Arial';
+    ctx.fillText(text, 200, 300);
+  
+    // Enviar a imagem como resposta
+    res.setHeader('Content-Type', 'image/png');
+    canvas.pngStream().pipe(res);
+  });
 
 module.exports = router;
